9102d22fe5c8c8505b88284e50c82354fbdfbbc1
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-09 00:06:41 +0200

    finish header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  ustoraImage: string = 'assets/logo.png';
+  price:number= 0
+  count:number=0
+  
 }
