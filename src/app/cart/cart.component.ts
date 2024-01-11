c1c8c7feab97418e59467bfdaf5424eaeab898a5
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-11 19:19:02 +0200

    adding cart component data

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchProductsComponent } from '../search-products/search-products.component';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [SearchProductsComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
