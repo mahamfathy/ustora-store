2c9c8b26cf9979d07b3baed3253000dbbaaa6f4c
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-12 23:43:07 +0200

    product services and removed db.json

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,21 +1,111 @@
-import { Injectable, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Product } from '../shared/models/Product';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ProductsService implements OnInit {
-  productsApiUrl = 'http://localhost:3000/products';
-
+export class ProductsService {
   constructor() {}
-  ngOnInit(): void {}
-  async getAllProducts():Promise<Product[]>{
-    const data = await fetch(this.productsApiUrl)
-    return await data.json()??[]
-  }
 
-  async getProductById(id: number): Promise<Product | undefined> {
-    const data = await fetch(`${this.productsApiUrl}/${id}`);
-    return (await data.json()) ?? {};
+  getAllProducts(): Product[] {
+    const products: Product[] = [
+      {
+        id: 1,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-1.jpg',
+        imageName: 'Apple New Mac',
+        price: 899,
+        discount: 999,
+      },
+      {
+        id: 2,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-2.jpg',
+        imageName: 'Apple New Mac',
+        price: 899,
+        discount: 999,
+      },
+      {
+        id: 3,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-3.jpg',
+        imageName: 'Apple New Mac',
+        price: 850,
+        discount: 999,
+      },
+      {
+        id: 4,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-4.jpg',
+        imageName: 'Apple New Mac',
+        price: 899,
+        discount: 999,
+      },
+      {
+        id: 5,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-1.jpg',
+        imageName: 'Apple New Mac',
+        price: 899,
+        discount: 999,
+      },
+      {
+        id: 6,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-2.jpg',
+        imageName: 'Apple New Mac',
+        price: 899,
+        discount: 999,
+      },
+      {
+        id: 7,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-3.jpg',
+        imageName: 'Apple New Mac',
+        price: 600,
+        discount: 999,
+      },
+      {
+        id: 8,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-4.jpg',
+        imageName: 'Apple New Mac',
+        price: 899,
+        discount: 999,
+      },
+      {
+        id: 9,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-1.jpg',
+        imageName: 'Apple New Mac',
+        price: 899,
+        discount: 999,
+      },
+      {
+        id: 10,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-2.jpg',
+        imageName: 'Apple New Mac',
+        price: 899,
+        discount: 999,
+      },
+      {
+        id: 11,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-3.jpg',
+        imageName: 'Apple New Mac',
+        price: 700,
+        discount: 999,
+      },
+      {
+        id: 12,
+        header: 'Apple new mac book 2015 March :P',
+        imageUrl: '../assets/product-4.jpg',
+        imageName: 'Apple New Mac',
+        price: 899,
+        discount: 999,
+      },
+    ];
+    return products;
   }
 }
