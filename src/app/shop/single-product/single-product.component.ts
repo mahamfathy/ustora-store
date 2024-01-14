2b0358c82ede19fcd7588085144c9ae7925d5b8b
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-14 23:34:14 +0200

    adjusting functions

--- src/app/shop/single-product/single-product.component.ts
+++ src/app/shop/single-product/single-product.component.ts
@@ -3,35 +3,44 @@ import { SearchProductsComponent } from '../../search-products/search-products.c
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Product } from '../../shared/models/Product';
 import { ProductsService } from '../../services/products.service';
+import { CartService } from '../../services/cart.service';
 @Component({
   selector: 'app-single-product',
   standalone: true,
-  imports: [SearchProductsComponent,RouterLink],
+  imports: [SearchProductsComponent, RouterLink],
   templateUrl: './single-product.component.html',
-  styleUrls: ['./single-product.component.css']
+  styleUrls: ['./single-product.component.css'],
 })
-export class SingleProductComponent implements OnInit{
-  productId :number|undefined
-  singleProduct:Product|undefined;
- 
-  constructor(private activatedRoute:ActivatedRoute,private productServer:ProductsService){}
+export class SingleProductComponent implements OnInit {
+  productId: number | undefined;
+  singleProduct: Product | undefined;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productServer: ProductsService,
+    private cartService: CartService
+  ) {}
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params => {
+    this.activatedRoute.params.subscribe((params) => {
       this.productId = +params['id']; // Convert to number
       this.fetchSingleProduct();
     });
   }
-  
-    
+
   private fetchSingleProduct(): void {
     if (this.productId !== undefined) {
-      this.productServer.getProductById(this.productId).then(
-        (data: Product | undefined) => {
+      this.productServer
+        .getProductById(this.productId)
+        .then((data: Product | undefined) => {
           this.singleProduct = data;
-        }
-      );
+        });
     }
   }
-    
-}
 
+  addToCart(e: Event): void {
+    e.preventDefault();
+    if (this.singleProduct) {
+      this.cartService.addToCart(this.singleProduct);
+    }
+  }
+}
