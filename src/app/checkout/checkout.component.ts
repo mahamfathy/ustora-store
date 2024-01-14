bcb31c4499ae739ce6e27fef256bcea11a95a18e
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-14 23:54:03 +0200

    finish checkout functions and html

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SearchProductsComponent } from '../search-products/search-products.component';
+import { CartService } from '../services/cart.service';
+import { Cart } from '../shared/models/Cart';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
   imports: [SearchProductsComponent],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrl: './checkout.component.css',
 })
-export class CheckoutComponent {
-
+export class CheckoutComponent implements OnInit {
+  cart!: Cart;
+  constructor(private cartService: CartService) {}
+  ngOnInit(): void {
+    this.cart = this.cartService.getCart();
+  }
 }
