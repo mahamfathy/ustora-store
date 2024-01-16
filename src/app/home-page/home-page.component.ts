f2b853cef20b7bd6099d03927d8923d145f7c9b4
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-16 02:47:38 +0200

    adjustments in latest-products comp

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,52 +1,15 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
 import { LatestProductsComponent } from '../latest-products/latest-products.component';
 import { CarouselComponent } from '../carousel/carousel.component';
-import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { CommonModule } from '@angular/common';
-import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [
-    FooterComponent,
-    HeaderComponent,
-    LatestProductsComponent,
-    CarouselComponent,
-    SlickCarouselModule,
-    CommonModule,
-  ],
+  imports: [LatestProductsComponent, CarouselComponent, CommonModule],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
-  images = [
-    { id: 1, src: 'assets/product-1.jpg', alt: 'Image 1' },
-    { id: 2, src: 'assets/product-2.jpg', alt: 'Image 2' },
-    { id: 3, src: 'assets/product-3.jpg', alt: 'Image 3' },
-    { id: 4, src: 'assets/product-4.jpg', alt: 'Image 4' },
-    { id: 5, src: 'assets/product-5.jpg', alt: 'Image 5' },
-    { id: 6, src: 'assets/product-6.jpg', alt: 'Image 6' },
-    // Add more objects as needed
-  ];
-  currentIndex = 0;
-
-  slideConfig = {
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: true,
-  };
   constructor() {}
-  next() {
-    if (this.currentIndex < this.images.length - 1) {
-      this.currentIndex++;
-    }
-  }
-  prev() {
-    if (this.currentIndex > 0) {
-      this.currentIndex--;
-    }
-  }
 }
--- src/app/latest-products/latest-products.component.ts
+++ src/app/latest-products/latest-products.component.ts
@@ -1,13 +1,72 @@
 import { Component } from '@angular/core';
-import { CarouselComponent } from '../carousel/carousel.component';
-import * as $ from 'jquery';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-latest-products',
   standalone: true,
-  imports: [CarouselComponent],
+  imports: [CarouselModule,ButtonModule,RouterLink],
   templateUrl: './latest-products.component.html',
-  styleUrl: './latest-products.component.css'
+  styleUrl: './latest-products.component.css',
 })
 export class LatestProductsComponent {
-
+  images = [
+    {
+      id: 1,
+      name: 'Samsung gallaxy s-5 2015',
+      src: 'assets/product-1.jpg',
+      imageName: 'image 1',
+      price: 800,
+      discount: 900,
+    },
+    {
+      id: 2,
+      name: 'Nokia Lumia 1320',
+      src: 'assets/product-2.jpg',
+      imageName: 'image 2',
+      price: 1100,
+      discount: 1500,
+    },
+    {
+      id: 3,
+      name: 'LG Leon 2015',
+      src: 'assets/product-3.jpg',
+      imageName: 'image 3',
+      price: 200,
+      discount: 500,
+    },
+    {
+      id: 4,
+      name: 'Sony microsoft',
+      src: 'assets/product-4.jpg',
+      imageName: 'image 4',
+      price: 550,
+      discount: 700,
+    },
+    {
+      id: 5,
+      name: 'Sony Smart Air Condtion',
+      src: 'assets/product-5.jpg',
+      imageName: 'image 5',
+      price: 900,
+      discount: 1000,
+    },
+    {
+      id: 6,
+      name: 'Samsung gallaxy note 4',
+      src: 'assets/product-6.jpg',
+      imageName: 'image 6',
+      price: 700,
+      discount: 800,
+    },
+    // Add more objects as needed
+  ];
+  responsiveOptions = [
+    {
+      breakpoint: '1600px',
+      numVisible: 5,
+      numScroll: 5,
+    },
+    // ... other responsive options
+  ];
 }
