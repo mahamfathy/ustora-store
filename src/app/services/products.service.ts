4d53a9a55cd12a051a65bd458ba960581032c353
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-20 23:53:06 +0200

    adding db.json file

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -5,117 +5,23 @@ import { Product } from '../shared/models/Product';
   providedIn: 'root',
 })
 export class ProductsService {
+  productsApiUrl = "http://localhost:3000/products"
    products: Product[] = [
-    {
-      id: 1,
-      name: 'Apple new mac book 2015 March :P',
-      src: '../assets/product-1.jpg',
-      imageName: 'Apple New Mac',
-      price: 899,
-      discount: 999,
-    },
-    {
-      id: 2,
-      name: 'Apple new mac book 2015 March :P',
-      src: '../assets/product-2.jpg',
-      imageName: 'Apple New Mac',
-      price: 899,
-      discount: 999,
-    },
-    {
-      id: 3,
-      name: 'Apple new mac book 2021 March :P',
-      src: '../assets/product-3.jpg',
-      imageName: 'Apple New Mac',
-      price: 750,
-      discount: 900,
-    },
-    {
-      id: 4,
-      name: 'Apple new mac book 2015 March :P',
-      src: '../assets/product-4.jpg',
-      imageName: 'Apple New Mac',
-      price: 899,
-      discount: 999,
-    },
-    {
-      id: 5,
-      name: 'New mac March :P',
-      src: '../assets/product-1.jpg',
-      imageName: 'Apple New Mac',
-      price: 450,
-      discount: 600,
-    },
-    {
-      id: 6,
-      name: 'Apple 2022 March :P',
-      src: '../assets/product-2.jpg',
-      imageName: 'Apple New Mac',
-      price: 550,
-      discount: 799,
-    },
-    {
-      id: 7,
-      name: 'mac book 2023 March :P',
-      src: '../assets/product-3.jpg',
-      imageName: 'Apple New Mac',
-      price: 600,
-      discount: 799,
-    },
-    {
-      id: 8,
-      name: 'Andriod :P',
-      src: '../assets/product-4.jpg',
-      imageName: 'Apple New Mac',
-      price: 499,
-      discount: 699,
-    },
-    {
-      id: 9,
-      name: 'Apple new mac book :P',
-      src: '../assets/product-1.jpg',
-      imageName: 'Apple New Mac',
-      price: 899,
-      discount: 999,
-    },
-    {
-      id: 10,
-      name: 'IPhone 2015 March :P',
-      src: '../assets/product-2.jpg',
-      imageName: 'Apple New Mac',
-      price: 600,
-      discount: 799,
-    },
-    {
-      id: 11,
-      name: ' mac book 2020 March :P',
-      src: '../assets/product-3.jpg',
-      imageName: 'Apple New Mac',
-      price: 700,
-      discount: 899,
-    },
-    {
-      id: 12,
-      name: 'Apple new mac book 2018 :P',
-      src: '../assets/product-4.jpg',
-      imageName: 'Apple New Mac',
-      price: 500,
-      discount: 900,
-    },
+
   ];
 
   carousel:Product[]= this.products.slice(0,6)
   constructor() {}
 
-  getAllProducts(): Product[] {
-   
-    return this.products;
+ async getAllProducts():Promise<Product[]> {
+   const data= await fetch(this.productsApiUrl)
+  return await data.json()??[]
   }
-  getProductById(id:number):Promise<Product|undefined>{
-    const product = this.products.find(p => p.id === id);
-
+ async getProductById(id:number):Promise<Product|undefined>{
+  const data = await fetch(`${this.productsApiUrl}/${id}`)
   
-    return Promise.resolve(product);
+  return await data.json()?? {}
+
 
 }
 getTotalProductsPrice(): number {
--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/models/Product';
 import { ProductsService } from '../services/products.service';
 import { RouterLink } from '@angular/router';
@@ -11,13 +11,16 @@ import { CartService } from '../services/cart.service';
   templateUrl: './shop.component.html',
   styleUrl: './shop.component.css',
 })
-export class ShopComponent {
-  products: Product[];
+export class ShopComponent implements OnInit {
+  products: Product[] = [];
   constructor(
     private productService: ProductsService,
     private cartService: CartService
-  ) {
-    this.products = productService.getAllProducts();
+  ) {}
+  ngOnInit(): void {
+    this.productService
+      .getAllProducts()
+      .then((products) => (this.products = products));
   }
   addToCart(item: Product) {
     this.cartService.addToCart(item);
