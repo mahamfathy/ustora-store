b41f74e589ea00539d9df1a06ed20fbd43daa83e
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-20 02:30:22 +0200

    adjusting in reactive form

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -2,29 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { SearchProductsComponent } from '../search-products/search-products.component';
 import { CartService } from '../services/cart.service';
 import { Cart } from '../shared/models/Cart';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [SearchProductsComponent,ReactiveFormsModule ],
+  imports: [SearchProductsComponent, ReactiveFormsModule],
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
   loginForm = new FormGroup({
     email: new FormControl('', Validators.required),
-    password: new FormControl('',Validators.required),
-    rememberme: new FormControl(false)
-    }
-  )
+    password: new FormControl('', Validators.required),
+    rememberme: new FormControl(false),
+  });
   cart!: Cart;
   constructor(private cartService: CartService) {}
   ngOnInit(): void {
     this.cart = this.cartService.getCart();
   }
-  onSubmit(){
-    console.log(`welcome, you have signed in ❤`)
+  onSubmit() {
+    console.log(`welcome, you have signed in ❤`);
+    const username = document.getElementById('username')! as HTMLInputElement;
+    const password = document.getElementById('password')! as HTMLInputElement;
+    username.value = '';
+    password.value = '';
   }
 }
