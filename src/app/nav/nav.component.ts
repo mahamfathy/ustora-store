7910af79062d6c6c80e708550810211377af63f8
mahafathy187@gmail.com <mahafathyh@gmail.com>
2024-01-22 23:37:47 +0200

    adding testing files

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [RouterLink,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent {
+  @ViewChild('navbar-expand-md', { static: true }) navbarElement!: ElementRef;
 
+  constructor() {}
+  closeNavbar() {
+    setTimeout(() => {
+      const isNavbarCollapsed =
+        this.navbarElement?.nativeElement.classList.contains('show');
+      if (isNavbarCollapsed) {
+        this.navbarElement.nativeElement.classList.remove('show');
+      }
+    }, 1000);
+  }
 }
